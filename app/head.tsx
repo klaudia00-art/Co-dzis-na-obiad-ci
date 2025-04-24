5bbf16dda914bbb2ec3d5f8cf339335f2c27f583
klaudia00-art <klaudia.jestem69@gmail.com>
2025-04-24 17:28:49 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Co dziś na obiad?</title>
+      <meta name="description" content="Strona z inspiracjami na codzienne obiady." />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7454469455266269"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  )
+}
